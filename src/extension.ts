f92af6439f33805465988a28ef9acce7f2776851
maorlevinshtein <maorlv52@gmail.com>
2025-08-11 20:37:52 +0300

    test this to see in github

--- src/extension.ts
+++ src/extension.ts
@@ -38,10 +38,6 @@ async function getUrisByScope(scope: Scope): Promise<vscode.Uri[]> {
   return strategies[scope]();
 }
 
-
-
-
-
 export function activate(context: vscode.ExtensionContext) {
   // ----- OPEN CUSTOM SEARCH -----
   context.subscriptions.push(
