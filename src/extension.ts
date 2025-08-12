398516272c965f8ec608e6a741cc93c60f227c47
maorlevinshtein <maorlv52@gmail.com>
2025-08-12 16:11:45 +0300

    asdasd

--- src/extension.ts
+++ src/extension.ts
@@ -188,8 +188,6 @@ export function activate(context: vscode.ExtensionContext) {
       null, 2
     );
 
-    
-
     await vscode.env.clipboard.writeText(snippet);
 
     const choice = await vscode.window.showInformationMessage(
