fabd0ed71ad42a76fb1a7128f27992baed3ebf2b
maorlevinshtein <maorlv52@gmail.com>
2025-08-12 08:41:30 +0300

    test

--- src/extension.ts
+++ src/extension.ts
@@ -39,6 +39,10 @@ async function getUrisByScope(scope: Scope): Promise<vscode.Uri[]> {
 }
 
 
+
+
+
+
 export function activate(context: vscode.ExtensionContext) {
   // ----- OPEN CUSTOM SEARCH -----
   context.subscriptions.push(
